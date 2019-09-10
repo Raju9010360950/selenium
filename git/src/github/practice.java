5874607f8547109fd658c164b50c297952708db9
user <user@user-PC>
2019-09-10 12:21:39 +0530

    new change

--- git/src/github/practice.java
+++ git/src/github/practice.java
@@ -2,8 +2,16 @@ package github;
 
 public class practice {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		int q;
+		int b = 0;
+		int c = 0;
+		q=b+c;
+		
+		System.out.println(q);
+		
 
 	}
 
