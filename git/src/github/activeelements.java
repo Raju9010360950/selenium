b3fdde41aed0a98f3c56b3f45714a1068c60eb18
user <user@user-PC>
2019-09-13 10:53:32 +0530

    new program

---
+++ git/src/github/activeelements.java
@@ -0,0 +1,20 @@
+package github;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class activeelements {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver=new FirefoxDriver();
+		WebElement element=driver.findElement(By.id(""));
+		
+		
+		
+	}
+
+}
